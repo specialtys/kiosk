e320e4b8e17f8519423e753056e28b1846846ac1
Vicky Payne <vicky@Ryan-Robleys-MacBook-Pro-4.local>
2014-09-02 15:37:26 -0700

    test

---
+++ test2/test2/HelloAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloAppDelegate.h
+//  test2
+//
+//  Created by Vicky Payne on 9/2/14.
+//  Copyright (c) 2014 VickyPayne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test2/test2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  test2
+//
+//  Created by Vicky Payne on 9/2/14.
+//  Copyright (c) 2014 VickyPayne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HelloAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HelloAppDelegate class]));
+    }
+}
